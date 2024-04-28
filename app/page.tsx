0b600261c907a4c608d6b64f9562522ce991bdaa
Zelfix <bastiankristiawan9@gmail.com>
2024-04-28 21:26:44 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
-
 export default function Home() {
   return (
 <>
-<div className="flex justify-end">
-  <div className="w-20">Enjoy being one with the nature like never before</div>
-<div className="font-bold text-9xl w-96">Stay With Us And Enjoy <span className="text-green-400">Nature</span></div>
+<div
+        className="w-full md:h-[90vh] h-[85vh] overflow-hidden relative"
+        style={{
+          backgroundImage: `url(${HeroBackground.src})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+<div className="flex flex-col items-center h-screen justify-center gap-y-2">
+<div className="font-bold text-7xl">Stay With Us And Enjoy <span className="text-green-400">Nature</span></div>
+  <div className="w-[]">Enjoy being one with the nature like never before</div>
 </div>
 </>
   );
