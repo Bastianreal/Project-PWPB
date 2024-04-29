71a0c58e6d2d5cd61d6dd7948348acdb91f5a677
Zelfix <bastiankristiawan9@gmail.com>
2024-04-29 20:56:05 +0800

    Added Hero Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
 import Image from "next/image";
+import Hero from "./Hero"
 export default function Home() {
   return (
 <>
-<div
-        className="w-full md:h-[90vh] h-[85vh] overflow-hidden relative"
-        style={{
-          backgroundImage: `url(${HeroBackground.src})`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
-<div className="flex flex-col items-center h-screen justify-center gap-y-2">
-<div className="font-bold text-7xl">Stay With Us And Enjoy <span className="text-green-400">Nature</span></div>
-  <div className="w-[]">Enjoy being one with the nature like never before</div>
-</div>
+<Hero />
 </>
   );
 }
